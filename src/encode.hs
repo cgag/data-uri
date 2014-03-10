626575bc48e782d88f48d6e2348864dbc17c8ff1
Curtis Gagliardi <cgag@riseup.net>
2014-03-09 23:30:30 -0700

    Simple encode64 fn, more complex main fn.

--- src/encode.hs
+++ src/encode.hs
@@ -4,13 +4,11 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import Data.ByteString.Base64 as Base64
 
-encodePath :: FilePath -> IO String
-encodePath path = do
-  file <- B.readFile path
-  let encoded = Base64.encode file
-  return $ BC.unpack encoded
+encode64 :: B.ByteString -> String
+encode64 = BC.unpack . Base64.encode 
 
 main = do
-  filePaths <- getArgs
-  encodedFiles <- mapM encodePath filePaths
-  mapM putStrLn encodedFiles
+ filePaths <- getArgs 
+ files <- mapM B.readFile filePaths
+ let encodedFiles = map encode64 files
+ mapM putStrLn encodedFiles
