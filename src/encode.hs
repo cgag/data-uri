91221c20e1e8bd7feb491c89d3e34f836722bf28
Curtis Gagliardi <cgag@riseup.net>
2014-03-10 21:58:53 -0700

    Build full data uris with correct handling of mimetypes.

--- src/encode.hs
+++ src/encode.hs
@@ -1,14 +1,33 @@
-import System.Environment
+{-# LANGUAGE OverloadedStrings #-}
 
+import System.Environment
+import qualified Data.Text as T
+import Text.Printf
+
+import Network.Mime
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import Data.ByteString.Base64 as Base64
 
+type Base64  = String
+type DataUri = String
+
 encode64 :: B.ByteString -> String
 encode64 = BC.unpack . Base64.encode 
 
+dataUri :: MimeType -> Base64 -> DataUri
+dataUri mime s = 
+  let m = filter (/='"') $ show mime 
+  in printf "data:%s;base64,%s" m s
+
+fromPath :: String -> IO DataUri
+fromPath path = do
+  let mime = defaultMimeLookup $ T.pack path
+  file <- B.readFile path
+  let encoded = encode64 file
+  return $ dataUri mime encoded
+
 main = do
- filePaths <- getArgs 
- files <- mapM B.readFile filePaths
- let encodedFiles = map encode64 files
- mapM putStrLn encodedFiles
+  filePaths <- getArgs
+  dataUris <- mapM fromPath filePaths
+  mapM putStrLn dataUris
